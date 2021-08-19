308fef4132fdc4df89316f12c70f42c09700944c
nastyatann <anastasia.kuz.84@gmail.com>
2021-08-19 11:03:56 +0300

    feat: add functions min, max, avg for Arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || !array.length) {
+    return 0;
+  }
+  let minElement = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] < minElement) {
+      minElement = array[i];
+    }
+  }
+  return minElement;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || !array.length) {
+    return 0;
+  }
+  let maxElement = array[0];
+  for (let i = 1; i < array.length; i++){
+    if (array[i] > maxElement) {
+      maxElement = array[i];
+    }
+  }
+  return maxElement;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || !array.length) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum/array.length;
 }
